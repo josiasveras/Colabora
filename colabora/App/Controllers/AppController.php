5b5056bd5c35ce05df3123b0caff694fb93c8edd
Jorzias Veras <josias_veras@hotmail.com>
2019-06-10 06:44:35 -0300

    updates diversos

--- colabora/App/Controllers/AppController.php
+++ colabora/App/Controllers/AppController.php
@@ -36,72 +36,110 @@
     
         }
 
-    public function fotos(){
+        public function fotos(){
 
-        $this->render('fotos');
+            $this->render('fotos');
 
-    }
+        }
 
-    public function quem_sou(){
+        public function quem_sou(){
 
-        $this->render('quem_sou');
+            $this->render('quem_sou');
 
-    }
+        }
 
-    public function projeto(){
+        public function projeto(){
 
-        session_start();
+            session_start();
 
-        //print_r ($_SESSION);
+            //print_r ($_SESSION);
+
+                if ($_SESSION['id'] != '' && $_SESSION['nome'] != '') {
+                    $this->render('projeto');
+                }else {
+                    header('Location: /login?auth=erro');
+                }
+
+        }
+
+        public function salvarProjeto() {
+
+            session_start();
+            
+            $this->render('projeto');
+            echo 'Cheguei aqui';
+            echo '<pre>';
+            print_r($_POST);
+            echo '</pre>';
 
             if ($_SESSION['id'] != '' && $_SESSION['nome'] != '') {
-                $this->render('projeto');
+                
+            $projeto = Container::getModel('Projeto');
+
+            echo 'Cheguei aqui 2';
+
+            /*$inputImagem = $_FILES['arquivo']['tmp_name'];
+            $imagem = fopen($inputImagem, "rb");
+            $img = fread($imagem, filesize($inputImagem));*/
+
+            $projeto->__set('id_categoria', $_POST['categoria']);
+            $projeto->__set('id_usuario', $_SESSION['id']);
+            $projeto->__set('nome_projeto', $_POST['nome_projeto']);
+            $projeto->__set('descricao', $_POST['Comment']);
+            //$projeto->__set('foto_projeto', $img);
+
+            $projeto->insertProjeto();
+            $this->render('projeto');
+
             }else {
                 header('Location: /login?auth=erro');
             }
+            
+        }
 
-    }
+        public function habilidades() {
 
-    public function salvarProjeto() {
-
-		session_start();
-		
-		$this->render('projeto');
-		echo 'Cheguei aqui';
-		echo '<pre>';
-		print_r($_POST);
-		echo '</pre>';
-
-		if ($_SESSION['id'] != '' && $_SESSION['nome'] != '') {
-			
-		$projeto = Container::getModel('Projeto');
-
-		echo 'Cheguei aqui 2';
-
-		/*$inputImagem = $_FILES['arquivo']['tmp_name'];
-    	$imagem = fopen($inputImagem, "rb");
-		$img = fread($imagem, filesize($inputImagem));*/
-
-		$projeto->__set('id_categoria', $_POST['categoria']);
-		$projeto->__set('id_usuario', $_SESSION['id']);
-		$projeto->__set('nome_projeto', $_POST['nome_projeto']);
-		$projeto->__set('descricao', $_POST['Comment']);
-		//$projeto->__set('foto_projeto', $img);
-
-		$projeto->insertProjeto();
-		$this->render('projeto');
-
-		}else {
-			header('Location: /login?auth=erro');
-		}
-		
-    }
+            $this->render('habilidades');
     
-    public function cadastro_editar() {
+        }
 
-		$this->render('cadastro_editar');
+        public function salvarHabilidade() {
 
-	}
+            session_start();
+            
+            $this->render('habilidades');
+            echo 'Cheguei aqui';
 
-}
+            if ($_SESSION['id'] != '' && $_SESSION['nome'] != '') {
+                
+            $habilidades = Container::getModel('Habilidades');
+
+            echo 'Cheguei aqui 2';
+
+            $habilidades->__set('usuario_cadastro_basico_id', $_SESSION['id']);
+            $habilidades->__set('habilidade_id', $_POST['div_esporte']);
+            $habilidades->__set('nivel', $_POST['user_skills']);
+
+            echo '<pre>';
+            print_r($_POST);
+            echo '</pre>';
+
+            return
+
+            $habilidades->insertHabilidades();
+            $this->render('habilidades');
+
+            }else {
+                header('Location: /login?auth=erro');
+            }
+            
+        }
+        
+        public function cadastro_editar() {
+
+            $this->render('cadastro_editar');
+
+        }
+
+    }
 ?>
--- colabora/BD_COLABORA.sql
+++ colabora/BD_COLABORA.sql
@@ -7,7 +7,7 @@ CREATE TABLE `usuario_cadastro_basico`
   `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `senha` varchar(8) NOT NULL,
+  `senha` varchar(32) NOT NULL,
   `dt_nasc` date NOT NULL,
   `genero` varchar(2) NOT NULL,
   `estado` varchar(2) NOT NULL
