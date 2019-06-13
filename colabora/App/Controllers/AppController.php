791e29d4e00f1f332c4bf19b05e7f4cede4c75c2
Jorzias Veras <josias_veras@hotmail.com>
2019-06-12 22:41:55 -0300

    Updates diversos
    Insert de uma habilidade no banco

--- colabora/App/Controllers/AppController.php
+++ colabora/App/Controllers/AppController.php
@@ -133,14 +133,14 @@
             echo 'Cheguei aqui 2';
 
             $habilidades->__set('usuario_cadastro_basico_id', $_SESSION['id']);
-            $habilidades->__set('habilidade_id', $_POST['user_preferences[reformas][]']);
-            $habilidades->__set('nivel', $_POST['user_skills']);
+            $habilidades->__set('habilidade_id', $_POST['habilidade_esporte']);
+            $habilidades->__set('nivel', $_POST['user_skills_esporte']);
 
             echo '<pre>';
             print_r($_POST);
             echo '</pre>';
 
-            //$habilidades->insertHabilidade();
+            $habilidades->insertHabilidade();
             $this->render('habilidades');
               
         }
